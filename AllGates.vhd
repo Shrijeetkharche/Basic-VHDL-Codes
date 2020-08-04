e7220768f14912511c246c98c3350e114b59efe0
Shrijeetkharche <46686763+Shrijeetkharche@users.noreply.github.com>
2020-08-04 19:43:00 +0530

    Add files via upload

---
+++ AllGates.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity AllGates is
+	port( a, b: in bit;
+			c, d, e, f, g, h, i : out bit);
+end AllGates;
+
+architecture Behavioral of AllGates is
+
+begin
+	c <= a and b;
+	d <= a or b;
+	e <= not a;
+	f <= a nand b;
+	g <= a nor b;
+	h <= a xor b;
+	i <= a xnor b;
+
+end Behavioral;
