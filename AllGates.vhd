112dfe97adb7674621700e98b588417b5bb2060a
Shrijeetkharche <46686763+Shrijeetkharche@users.noreply.github.com>
2020-08-04 19:43:41 +0530

    Update AllGates.vhd

--- AllGates.vhd
+++ AllGates.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity AllGates is
 	port( a, b: in bit;
-			c, d, e, f, g, h, i : out bit);
+	      c, d, e, f, g, h, i : out bit);
 end AllGates;
 
 architecture Behavioral of AllGates is
